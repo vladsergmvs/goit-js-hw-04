167c6f5e834e14c47724ceddcde24259253915bd
Volodymyr <72503462+vladsergmvs@users.noreply.github.com>
2024-06-22 01:57:01 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,18 @@
+
+function filterArray(numbers, value){
+    const resultArr = [];
+    for (const number of numbers) {
+        if (number > value) {
+            resultArr.push(number);
+        }
+    }
+    return resultArr;
+}
+ 
+console.log("===================  task-3  ======================"); 
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
